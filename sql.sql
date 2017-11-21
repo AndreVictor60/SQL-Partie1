ff053059d7336c055c42c2af957cf7bd3b3bffc5
AndreVictor60 <andre.victor60@free.fr>
2017-11-21 16:23:56 +0100

    fini

---
+++ sql.sql
@@ -0,0 +1,7 @@
+create database languages;
+create database webDevelopment character set 'utf8';
+create database frameworks character set 'utf8';
+create database languages character set 'utf8';
+drop database languages;
+drop database if exists frameworks;
+drop database if exists languages;
